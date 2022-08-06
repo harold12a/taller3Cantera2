89360c996406d85e4c1e4aec5f616614fc046378
harold alzate <haroldalzate99@gmail.com>
2022-08-06 11:19:33 -0500

    primer parte

---
+++ src/main/java/org/example/Biblioteca.java
@@ -0,0 +1,90 @@
+package org.example;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Biblioteca {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Canciones> listaDeCanciones = new ArrayList<>();
+        Canciones cancion1 = new Canciones("eres mia","Romeo Santos",new Date(114,10,5), new Time(0,3,15),"Bachate","XXX","una pareja rompe y ella rehace su vida. Él, lejos de asumir la ruptura, decide acosarla, intimidarla");
+        Canciones cancion2 = new Canciones("Destino o Casualidad ","Melendi",new Date(116,6,22),new Time(0,2,15),"pop","XXX","Ella iba caminando sola por la calle Pensando,Dios, ¡qué complicado es esto del amor!");
+        Canciones cancion3 = new Canciones("Las locuras mia","Silvestre",new Date(120,5,15),new Time(0,3,00),"Vallenato","XXX","Por Dios, que no quisiera sentir esta ilusión Tal vez podría cometer un grave error Porque tú eres prohibida para mí");
+        Canciones cancion4 = new Canciones("El precio del error","Luis Alberto Posada",new Date(98,4,15),new Time(0,4,00),"Despecho","XXX","No lo puedo creer Que esto le esté pasando a usted Que después de tanto alarde Después de tantos desaires Seas tú la tenga que volver");
+        Canciones cancion5 = new Canciones("las estrellas","Manuel medrano",new Date(116,6,22),new Time(0,4,00),"pop","XXX","Buscando en las estrellas encontré unos ojos tan brillantes como nada que conozcas más allá que el sol porque eres tan bella Más que el cielo y las estrellas tú eres lo que faltaba y siempre quise que estuviera");
+
+
+        int opcion = 0;
+        int numeroC;
+        String cancion;
+
+        do{
+            System.out.println("menu de opciones elige la opcion a realizar");
+            System.out.println(" 1- Crear playList");
+            System.out.println(" 2- filtrar Canciones Mismo Genero");
+            System.out.println(" 3- filtrar Canciones en el mismo año");
+            System.out.println(" 4- duracion y fecha de las Canciones");
+            System.out.println(" 5- salir");
+            System.out.println("INGRESA LA OPCION:");
+            opcion = scanner.nextInt();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("TENEMOS LAS DIGIENTES CANCIONES DISPONIBLES");
+                    listaDeCanciones.add(cancion1);
+                    listaDeCanciones.add(cancion2);
+                    listaDeCanciones.add(cancion3);
+                    listaDeCanciones.add(cancion4);
+                    listaDeCanciones.add(cancion5);
+                    for (int i = 0; i <listaDeCanciones.size() ; i++) {
+                        System.out.println("Cancion: " +listaDeCanciones.get(i).getTitulo());
+                    }
+                    System.out.println("INDIQUE LA CANTIDAD DE CANCIONES QUE  DESEAS AGREGAR A LA PLAYLIST");
+                    numeroC = scanner.nextInt();
+                    if (numeroC <=5 ){
+                        for (int i = 0; i <=numeroC ; i++) {
+                            System.out.println("INGRESE NOMBRE DE LA CANCION: ");
+                            cancion = scanner.nextLine();
+                        }
+
+
+                    }
+
+
+                    break;
+                case 2:
+                    System.out.println("GENERO DE LAS CANCIONES");
+                    listaDeCanciones.add(cancion1);
+                    listaDeCanciones.add(cancion2);
+                    listaDeCanciones.add(cancion3);
+                    listaDeCanciones.add(cancion4);
+                    listaDeCanciones.add(cancion5);
+                    for (int i = 0; i <listaDeCanciones.size() ; i++) {
+                        System.out.println("genero: "+ listaDeCanciones.get(i).getGenero());
+                    }
+
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    for (int i = 0; i <listaDeCanciones.size() ; i++) {
+                        System.out.println("genero: "+ listaDeCanciones.get(i).getDuracion()+" ---"+.getFe);
+                    }
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("***OPCION NO DISPONIBLE INGRESA OTRA OPCION***");
+                    break;
+
+            }
+
+        }while(opcion !=4);
+
+
+    }
+}
---
+++ src/main/java/org/example/Canciones.java
@@ -0,0 +1,107 @@
+package org.example;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Canciones {
+    public String titulo;
+    public String identificador;
+    public Date fecha;
+    public Time duracion;
+    public String genero;
+    public String caratula;
+    public String descripcion;
+
+    public Canciones(String titulo, String identificador, Date fecha, Time duracion, String genero, String caratula, String descripcion) {
+        this.titulo = titulo;
+        this.identificador = identificador;
+        this.fecha = fecha;
+        this.duracion = duracion;
+        this.genero = genero;
+        this.caratula = caratula;
+        this.descripcion = descripcion;
+
+    }
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public Time getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Time duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCaratula() {
+        return caratula;
+    }
+
+    public void setCaratula(String caratula) {
+        this.caratula = caratula;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     *
+     * @return la fecha especificada asignada
+     */
+    public String formatearFecha (){
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy");
+        String fechaCadena=sdf.format(this.fecha);
+        return fechaCadena;
+    }
+
+    @Override
+    public String toString() {
+        return "Canciones{" +
+                "titulo='" + titulo + '\'' +
+                ", identificador='" + identificador + '\'' +
+                ", fecha=" + formatearFecha() +
+                ", duracion=" + duracion +
+                ", genero='" + genero + '\'' +
+                ", caratula='" + caratula + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+}
